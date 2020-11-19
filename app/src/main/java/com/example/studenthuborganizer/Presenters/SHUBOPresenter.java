cd2624472dc3462b24e2c64b3e17c7237b91309f
John Dong <mahoushisaber@gmail.com>
2020-11-19 11:10:37 -0800

    Bug fixed
    - Test code deleted for it to run properly

--- app/src/main/java/com/example/studenthuborganizer/Presenters/SHUBOPresenter.java
+++ app/src/main/java/com/example/studenthuborganizer/Presenters/SHUBOPresenter.java
@@ -44,44 +44,6 @@ public class SHUBOPresenter {
 
         // Add any code required
 
-
-        ArrayList<Long> AllRecords = model.GetRecordIDs();
-        if (AllRecords.size() > 0) {
-            // For testing we want no events so delete any found
-            for (int i = 0; i < AllRecords.size(); i++) {
-                if (model.DeleteRecord(AllRecords.get(i)) == false){
-                    Log.d("Testing SHUBOModel", "Failed to delete RecID: " + AllRecords.get(i));
-                }
-            }
-        }
-        String Title = "Test Event ";
-        String Description = "Test Description ";
-        String CourseName = "COMP-7082-0 of ";
-        boolean IsGroup = false;
-
-        //
-        // Ugly way to create a record but you may need to do it this way for
-        // event on same day 2020-11-18 from 18:00 to 19:00 hours
-        //
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2020, 11, 18, 18, 00);
-        long StartDate = beginTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2020, 11, 18, 19, 00);
-        long EndDate = endTime.getTimeInMillis();
-
-        SHUBORecord ARec1 = new SHUBORecord(-1, Title + 1, Description + 1, CourseName + "_1", IsGroup, StartDate, EndDate);
-        ARec1.RecordID = model.AddRecord(ARec1);
-
-        //
-        // Create a record without start and end dates in constructor than manual add
-        // an event from: 2020-11-20 13:30 to: 2020-11-20 15:50
-        //
-        SHUBORecord ARec2 = new SHUBORecord(-1, Title + 2, Description + 2, CourseName + "_2", IsGroup);
-        ARec2.SetStartDate(2020, 11, 20, 13, 30);
-        ARec2.SetEndDate(2020, 11, 20, 15, 50);
-        ARec2.RecordID = model.AddRecord(ARec2);
-
         // this must be called last
         model.StartInitial();
     }
