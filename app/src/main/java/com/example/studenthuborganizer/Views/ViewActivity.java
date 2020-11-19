a2b092a3c31fcac5a00491e57d3c28d915e2f151
Jaedin D <jaedindhatt@gmail.com>
2020-11-18 20:54:02 -0800

    dynamically add records

--- app/src/main/java/com/example/studenthuborganizer/Views/ViewActivity.java
+++ app/src/main/java/com/example/studenthuborganizer/Views/ViewActivity.java
@@ -1,8 +1,13 @@
 package com.example.studenthuborganizer.Views;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -23,11 +28,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studenthuborganizer.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 import org.w3c.dom.Text;
 
@@ -39,14 +47,40 @@ public class ViewActivity extends AppCompatActivity {
 
         List<String[]> myList = new ArrayList<>();
         myList.add(new String[]{"11/18", "Assignment 1", "MATH7908"});
+        myList.add(new String[]{"11/19", "Assignment 2", "MATH7908"});
+        myList.add(new String[]{"11/20", "Assignment 3", "COMP7082"});
 
         for (int i = 0; i < myList.size(); i++) {
-            CheckBox dueDate = (CheckBox)findViewById(R.id.checkBox);
-            TextView tv1 = (TextView)findViewById(R.id.assignmentText);
-            TextView tv2 = (TextView)findViewById(R.id.courseText);
+            //CheckBox dueDate = (CheckBox)findViewById(R.id.checkBox);
+            //TextView tv1 = (TextView)findViewById(R.id.assignmentText);
+            //TextView tv2 = (TextView)findViewById(R.id.courseText);
+            CheckBox dueDate = new CheckBox(this);
+            TextView assignmentName = new TextView(this);
+            TextView courseName = new TextView(this);
+            dueDate.setId(i);
+            assignmentName.setId(i);
+            courseName.setId(i);
             dueDate.setText(myList.get(i)[0]);
-            tv1.setText(myList.get(i)[1]);
-            tv2.setText(myList.get(i)[2]);
+            assignmentName.setText(myList.get(i)[1]);
+            assignmentName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            assignmentName.setTypeface(null, Typeface.BOLD);
+            assignmentName.setTextColor(Color.parseColor("#000000"));
+            assignmentName.setPadding(0, 20, 0, 0);
+            courseName.setText(myList.get(i)[2]);
+            courseName.setPadding(0, 20, 0, 0);
+
+            ChipGroup chipG = new ChipGroup(this);
+            chipG.setSingleLine(true);
+            chipG.setLayoutParams(new ChipGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 150));
+            chipG.setChipSpacingHorizontal(20);
+            chipG.setId(i);
+            chipG.addView(dueDate, new ViewGroup.LayoutParams(220, 100));
+            chipG.addView(assignmentName, new ViewGroup.LayoutParams(500, 100));
+            chipG.addView(courseName, new ViewGroup.LayoutParams(300, 100));
+
+            LinearLayout ll = (LinearLayout)findViewById(R.id.LinearLayout);
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            ll.addView(chipG, llp);
         }
 
     }
