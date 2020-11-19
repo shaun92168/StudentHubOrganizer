1fef1b348cbb742ac5c7d80d38e848803ab4642a
Jaedin D <jaedindhatt@gmail.com>
2020-11-18 21:50:42 -0800

    added deleteRecords function

--- app/src/main/java/com/example/studenthuborganizer/Views/ViewActivity.java
+++ app/src/main/java/com/example/studenthuborganizer/Views/ViewActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -51,15 +53,16 @@ public class ViewActivity extends AppCompatActivity {
         myList.add(new String[]{"11/20", "Assignment 3", "COMP7082"});
 
         for (int i = 0; i < myList.size(); i++) {
-            //CheckBox dueDate = (CheckBox)findViewById(R.id.checkBox);
-            //TextView tv1 = (TextView)findViewById(R.id.assignmentText);
-            //TextView tv2 = (TextView)findViewById(R.id.courseText);
             CheckBox dueDate = new CheckBox(this);
             TextView assignmentName = new TextView(this);
             TextView courseName = new TextView(this);
-            dueDate.setId(i);
-            assignmentName.setId(i);
-            courseName.setId(i);
+            ChipGroup chipG = new ChipGroup(this);
+
+            dueDate.setId(0);
+            //assignmentName.setId(i);
+            //courseName.setId(i);
+            //chipG.setId(i);
+
             dueDate.setText(myList.get(i)[0]);
             assignmentName.setText(myList.get(i)[1]);
             assignmentName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
@@ -69,11 +72,9 @@ public class ViewActivity extends AppCompatActivity {
             courseName.setText(myList.get(i)[2]);
             courseName.setPadding(0, 20, 0, 0);
 
-            ChipGroup chipG = new ChipGroup(this);
             chipG.setSingleLine(true);
             chipG.setLayoutParams(new ChipGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 150));
             chipG.setChipSpacingHorizontal(20);
-            chipG.setId(i);
             chipG.addView(dueDate, new ViewGroup.LayoutParams(220, 100));
             chipG.addView(assignmentName, new ViewGroup.LayoutParams(500, 100));
             chipG.addView(courseName, new ViewGroup.LayoutParams(300, 100));
@@ -91,16 +92,20 @@ public class ViewActivity extends AppCompatActivity {
     }
 
     public void deleteRecords(final View v) {
-        Intent i = new Intent();
-        setResult(RESULT_OK, i);
-        finish();
-    }
+        List<Integer> recordsToRemove = new ArrayList<>();
+        LinearLayout ll = (LinearLayout)findViewById(R.id.LinearLayout);
+        for (int i = 0; i < ll.getChildCount(); i++) {
+            View cg = ll.getChildAt(i);
+            CheckBox cb = cg.findViewById(0);
+            if (cb.isChecked()) {
+                recordsToRemove.add(i);
+            }
+        }
 
-    public void addItemToRemove(final View v) {
-
-    }
-
-    public void removeItemToRemove(final View v) {
+        Collections.sort(recordsToRemove, Collections.reverseOrder());
+        for (int i = 0; i < recordsToRemove.size(); i++) {
+            ll.removeViewAt(recordsToRemove.get(i));
+        }
 
     }
 
